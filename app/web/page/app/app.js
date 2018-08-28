669f02c3a88f5d7a7a535c82d4501db78db40afe
Jie Ren Tian <110873204@qq.com>
2018-08-28 09:22:30 +0800

    mint组件的引入方式修改，部分编译问题的解决.

--- app/web/page/app/app.js
+++ app/web/page/app/app.js
@@ -1,10 +1,16 @@
 import { sync } from 'vuex-router-sync';
+import Vue from 'vue';
 import store from 'store/store';
 import router from 'component/app/router';
 import app from './app.vue';
 import App from 'app';
 import Layout from 'component/layout/app';
 
+
+import MintUI from 'mint-ui';
+import 'mint-ui/lib/style.css';
+
+Vue.use(MintUI);
 App.component(Layout.name, Layout);
 
 sync(store, router);
