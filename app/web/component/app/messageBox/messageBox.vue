a3b9c31f96138c7f7cf8523f7df41613a5b7d9a7
Jie Ren Tian <110873204@qq.com>
2018-09-03 10:05:07 +0800

    消息列表组件messageBox组件的新增.

---
+++ app/web/component/app/messageBox/messageBox.vue
@@ -0,0 +1,17 @@
+<template></template>
+<script type="text/babel">
+  /**
+   * 消息列表组件
+   */
+  export default {
+    name: 'message-box',
+    components: {},
+    data() {
+      return {};
+    },
+    computed: {},
+    watch: {},
+    methods: {},
+  };
+</script>
+<style lang="less"></style>
