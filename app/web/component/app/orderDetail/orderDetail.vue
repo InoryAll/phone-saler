f44780180a0905321bdf48afeb1e9665e6699ac6
Jie Ren Tian <110873204@qq.com>
2018-09-13 17:06:22 +0800

    orderDetail组件新增.订单详情组件.

---
+++ app/web/component/app/orderDetail/orderDetail.vue
@@ -0,0 +1,18 @@
+<template></template>
+<script type="text/babel">
+  /**
+   * 订单详情页组件
+   */
+
+  export default {
+    name: 'order-detail',
+    components: {},
+    data() {
+      return {};
+    },
+    computed: {},
+    watch: {},
+    methods: {},
+  };
+</script>
+<style lang="less"></style>
