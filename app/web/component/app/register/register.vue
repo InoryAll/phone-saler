7c21cec1b4ff1159391ed43de2a12e7389475229
Jie Ren Tian <110873204@qq.com>
2018-09-19 11:28:26 +0800

    register组件的设计以及完成，用户注册组件.

--- app/web/component/app/register/register.vue
+++ app/web/component/app/register/register.vue
@@ -9,7 +9,7 @@
         <a class="phone-register-header-left" @click="handlePopUpClose">
           <i class="iconfont icon-left header-left-icon"></i>
         </a>
-        <span class="phone-register-header-span">手机密码登录</span>
+        <span class="phone-register-header-span">用户注册</span>
       </div>
       <div class="phone-register-content">
         <div class="phone-register-content-logo">
@@ -20,20 +20,37 @@
             <label for="username" class="phone-register-form-label">
               <i class="iconfont icon-user phone-register-form-icon"></i>
             </label>
-            <mt-field label="" :attr="{ id: 'username', name: 'username' }" placeholder="请输入手机号" type="tel"></mt-field>
+            <mt-field label="" :attr="{ id: 'reg-username', name: 'reg-username' }" placeholder="请输入手机号" type="tel"></mt-field>
           </div>
           <div class="form-item clearfix">
             <label for="password" class="phone-register-form-label">
               <i class="iconfont icon-lock phone-register-form-icon"></i>
             </label>
-            <mt-field label="" :attr="{ id: 'password', name: 'password' }" placeholder="请输入密码" type="password"></mt-field>
+            <mt-field label="" :attr="{ id: 'reg-password', name: 'reg-password' }" placeholder="请输入密码" type="password"></mt-field>
+          </div>
+          <div class="form-item clearfix">
+            <label for="captcha" class="phone-register-form-label">
+              <i class="iconfont icon-key phone-register-form-icon"></i>
+            </label>
+            <mt-field label="" :attr="{ id: 'captcha', name: 'captcha' }" placeholder="验证码">
+              <mt-button
+                @click.native="handleCaptchaClick"
+                class="form-item-btn"
+                type="default"
+                :disabled="captchaDisabled"
+              >
+                <div slot="icon" v-if="captchaDisabled">
+                  <mt-spinner type="fading-circle"></mt-spinner>
+                </div>
+                {{captchaText}}
+              </mt-button>
+            </mt-field>
           </div>
         </form>
         <div class="phone-register-form-btn">
-          <button class="form-btn-login">登录</button>
+          <button class="form-btn-login">注册</button>
           <div class="clearfix">
-            <a href="" class="form-btn-forget">找回密码</a>
-            <a href="" class="form-btn-register">还没有账号，立即注册 &gt;&gt;</a>
+            <a @click="handlePopUpClose" class="form-btn-register">已有账号，立即登录 &gt;&gt;</a>
           </div>
         </div>
       </div>
@@ -44,13 +61,14 @@
   /**
    * 登录组件
    */
-  import { Popup, Field } from 'mint-ui';
+  import { Popup, Field, Button } from 'mint-ui';
 
   export default {
     name: 'register-page',
     components: {
       Popup,
-      Field
+      Field,
+      Button
     },
     props: {
       registerVisible: {
@@ -61,6 +79,9 @@
     data() {
       return {
         visible: false,
+        captchaText: '发送验证码',
+        captchaDisabled: false,
+        count: 60,
       };
     },
     computed: {},
@@ -80,6 +101,23 @@
       handlePopUpClose() {
         this.visible = false;
       },
+      handleCaptchaClick() {
+        if (this.count > 0) {
+          this.captchaDisabled = true;
+          this.captchaText = this.count + 's';
+        }
+        const timer = setInterval(() => {
+          if (this.count > 0) {
+            this.count--;
+            this.captchaText = this.count + 's';
+          } else {
+            clearInterval(timer);
+            this.count = 60;
+            this.captchaDisabled = false;
+            this.captchaText = '发送验证码';
+          }
+        }, 1000);
+      }
     },
   };
 </script>
@@ -124,8 +162,16 @@
       .form-item{
         border: 1px solid #ccc;
         margin: 10px 0;
-        padding: 0 20px;
+        padding-left: 20px;
+        padding-right: 4px;
         border-radius: 8px;
+        &-btn{
+          font-size: 14px;
+          .mint-button-text{
+            margin-bottom: 0;
+            font-weight: normal;
+          }
+        }
       }
       &-label{
         float: left;
