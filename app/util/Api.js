0f2f24105d6a425a08811049e831072c78ac3415
Jie Ren Tian <110873204@qq.com>
2018-09-25 15:11:44 +0800

    基于axios封装的上层api请求类Api

---
+++ app/util/Api.js
@@ -0,0 +1,20 @@
+/**
+ * 请求API上层封装
+ * Created by tianrenjie on 2018/9/25
+ */
+const axios = require('axios');
+
+class Api {
+  static createAxiosAPI(options) {
+    return (data) => {
+      axios({
+        method: options.method,
+        url: options.url,
+        data: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+      });
+    };
+  }
+}
+
+module.exports = Api;
